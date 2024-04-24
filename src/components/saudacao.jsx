05c652cbf3f8cca98298b8aea7550b3faed6b3cc
Mlimac <mucruzlima272@gmail.com>
2024-04-24 20:58:29 -0300

    primeiro commit

---
+++ src/components/saudacao.jsx
@@ -0,0 +1,6 @@
+function Saudacao(prop){
+    return <h1>Bem vindo à CMO,{prop.nome}</h1>
+}
+
+export default Saudacao;
+
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Saudacao from './components/saudacao';
+//Componetização
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<>
+ <Saudacao nome='Murilo'/>
+<p>Voce esta na pagina CMO</p>
+</>
+);
