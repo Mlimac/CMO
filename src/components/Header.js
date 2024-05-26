79ad4cac79f7067fb938b30313e0cbfb0b570135
FHDOLBERTH <dolberthfelipe@hotmail.com>
2024-05-25 21:46:44 -0300

    att.5

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,81 +1,96 @@
-import './styles/Header.css'
-import logo from './styles/images/logocmo.png'
-import user from './styles/images/Usuario.png'
-import './Page1'
-import './Modal'
+import React, { useState, useEffect } from 'react';
+import './styles/Header.css'; // Importe o arquivo de estilo CSS
 
-class MobileNavbar{
-  constructor(mobileMenu, navList, navLinks){
+import logo from './styles/images/logocmo.png';
+import user from './styles/images/Usuario.png';
+
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
     this.mobileMenu = document.querySelector(mobileMenu);
     this.navList = document.querySelector(navList);
     this.navLinks = document.querySelectorAll(navLinks);
     this.activeClass = 'active';
 
-    this.handleClick = this.handleClick.bind()
+    this.handleClick = this.handleClick.bind(this);
   }
 
- handleClick(){
-  console.log(this);
-  this.navList.classList.toggle(this.activeClass)
- }
-
-  addClickEvent(){
-      this.mobileMenu.addEventListener('click', this.handleClick)
+  handleClick() {
+    console.log(this);
+    this.navList.classList.toggle(this.activeClass);
   }
 
-  init(){
-      if (this.mobileMenu){
-          this.addClickEvent();
-      }
-  
-  return this;
+  addClickEvent() {
+    this.mobileMenu.addEventListener('click', this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+
+    return this;
   }
 }
 
-const mobileNavbar = new MobileNavbar(
-  '.mobile-menu',
-  '.nav-list',
-  '.nav-list li',
-
-)
+const mobileNavbar = new MobileNavbar('.mobile-menu', '.nav-list', '.nav-list li');
 mobileNavbar.init();
 
-function Header(){
-  
-    return (
-      <nav>
-    <div className="header">
-      <div className="header__left">
-       <img className='logo' src={logo} alt="logo"></img>
-       <h2>Casa do Microondas</h2>
-      </div>
-      <div className='mobile-menu'>
-         <div className='line1'></div>
-         <div className='line2'></div>
-         <div className='line3'></div>
-      </div>
-      <div className="header__center">
-        <ul className='header__lists'>
-            <li><a href='#home'>Home</a></li>
-            <li><a href='#servicos'>Serviços</a></li>
-            <li><a href='#localizacao'>Localização</a></li>
-            
-    </ul>
-      </div>
-      <div className="header__right">
-        
-      </div>
-      <div className='user login'>
-  
-    <a href='login user'>
-      <img className='perfil' src={user} alt="usuario" />
-    </a>
-  
-</div>
+function Header() {
+  const [buttonsVisible, setButtonsVisible] = useState(false);
 
-    </div>
+  const toggleButtons = () => {
+    setButtonsVisible(!buttonsVisible);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setButtonsVisible(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [buttonsVisible]);
+
+  return (
+    <nav>
+      <div className="header">
+        <div className="header__left">
+          <img className="logo" src={logo} alt="logo" />
+          <h2>Casa do Microondas</h2>
+        </div>
+        <div className="mobile-menu">
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+        <div className="header__center">
+          <ul className="header__lists">
+            <li>
+              <a href="#home" style={{ scrollBehavior: 'smooth' }}>Home</a>
+            </li>
+            <li>
+              <a href="#servicos" style={{ scrollBehavior: 'smooth' }}>Serviços</a>
+            </li>
+            <li>
+              <a href="#localizacao" style={{ scrollBehavior: 'smooth' }}>Localização</a>
+            </li>
+          </ul>
+        </div>
+        <div className="header__right">
+          
+          <div className="user" onClick={toggleButtons}>
+            <img className="perfil" src={user} alt="usuario" />
+            
+            {buttonsVisible && (
+              <div className="user-buttons">
+                <button className="login-button">Login</button>
+                <button className="signup-button">Cadastro</button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </nav>
-    )
+  );
 }
 
-export default Header
+export default Header;
